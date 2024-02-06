4d13422ba60ec03c134736189a3f5c7bb66dd28d
pamplona-c <gustavo.pamplona.549@gmail.com>
2024-02-06 08:38:56 -0300

    Criação da header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from "@/components/header"
 import {View, Text} from "react-native"
 
 
 export default function Home(){
     return(
         <View className="flex-1 pt-8">
-            <Text className="text-white text-2xl">Hello World React Native</Text>
+            <Header title="Cardapio" cartQuantityItems={5}/>
         </View>
     )
 }
