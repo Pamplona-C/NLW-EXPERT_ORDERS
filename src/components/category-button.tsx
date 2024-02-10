5e81977ecbd7c91b16e6a3ace7a09c2bfe239ab3
pamplona-c <gustavo.pamplona.549@gmail.com>
2024-02-10 16:21:14 -0300

    Criação de categoria

---
+++ src/components/category-button.tsx
@@ -0,0 +1,14 @@
+import {Text, Pressable, PressableProps} from "react-native"
+
+type CategoryProps = PressableProps & {
+    title: string
+    isSelected?: boolean
+}
+
+export function CategoryButton({title, isSelected}: CategoryProps){
+    return(
+        <Pressable className="bg-slate-800 px-4 justify-center rounded-md h-10">
+            <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
+        </Pressable>
+    )
+}
