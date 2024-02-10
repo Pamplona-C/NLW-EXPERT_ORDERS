503474881dd4166ce5bf03782fca527cb0699b2d
pamplona-c <gustavo.pamplona.549@gmail.com>
2024-02-10 16:58:11 -0300

    Criação de categorias, dinamica

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,34 @@
 import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
-import {View, Text} from "react-native"
+import {View, FlatList} from "react-native"
+import {CATEGORIES} from '@/utils/data/products'
+import { useState } from "react"
 
 
 export default function Home(){
+
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+    }
+
     return(
         <View className="flex-1 pt-8">
             <Header title="Cardapio" cartQuantityItems={1}/>
 
-            <CategoryButton title="Lanche do dia"/>
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <CategoryButton title={item} isSelected={item===category} onPress={() => handleCategorySelect(item)}/>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                className="max-h-10 mt-5"
+                contentContainerStyle={{gap:12, paddingHorizontal:20}}
+               
+            />
         </View>
     )
 }
